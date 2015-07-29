0f0e950d7b60de615909e8dcab09d9d9d6b53af7
Florence Chilisa <fchilisa@fchilisa.bhp.org>
2015-07-29 17:35:06 +0200

    pep8

--- manage.py
+++ manage.py
@@ -2,15 +2,14 @@
 import os
 import sys
 
-SOURCE_DIR = '/home/django/source'#os.path.expanduser('~/source')
+SOURCE_DIR = '/home/django/source'  # os.path.expanduser('~/source')
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apps.settings")
 
-    #sys.path.insert(1, os.path.join(SOURCE_DIR, 'edc_project/'))
-    #sys.path.insert(1, os.path.join(SOURCE_DIR, 'lis_project/'))
+    # sys.path.insert(1, os.path.join(SOURCE_DIR, 'edc_project/'))
+    # sys.path.insert(1, os.path.join(SOURCE_DIR, 'lis_project/'))
 
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-#The END
